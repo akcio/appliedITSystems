610e6d9ae2e5317c5ba315fd3787e13efeb3db2d
akcio <akcio@qip.ru>
2018-09-24 08:49:14 +0500

    * Другой подход для получения xml

--- freeling.py
+++ freeling.py
@@ -2,6 +2,7 @@ from lxml import etree
 from pyfreeling import Analyzer
 from os import path
 import subprocess
+import bs4
 
 CONFIG_FOLDER = path.join(path.dirname(__file__), 'freeling/data/config/')
 FILE_FOLDER = path.join(path.dirname(__file__), 'freeling/')
@@ -9,26 +10,27 @@ FILE_FOLDER = path.join(path.dirname(__file__), 'freeling/')
 inputText=open(path.join(FILE_FOLDER, 'test'))
 inputText=inputText.read()
 
-analyzer = Analyzer(config=path.join(CONFIG_FOLDER, 'ru.cfg'))
-print('start analyze')
-xml = analyzer.run(inputText.encode())
-root = xml#etree.parse(xml)
-rows = root.findall('sentence')
-nlist = {}
-exceptions = ''
-for s in rows:
-    tok = s.findall('token')
+p = subprocess.Popen('analyze -f ' + path.join(CONFIG_FOLDER, 'ru.cfg') + ' --output xml < ' + path.join(FILE_FOLDER, 'test'),
+                     stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+r = p.communicate()
+t = r[0]
+doc = bs4.BeautifulSoup(t, 'html.parser')
+sen = doc.find_all('sentence')
+nlist={}
+exceptions=str()
+for s in sen:
+    tok = s.find_all('token')
     for t in tok:
-        tag = t.attrib['tag']
+        tag = t.attrs['tag']
         try:
             if tag[1]!='P' and tag[0] == 'N' and tag[2] == 'N':
-                if t.attrib['form'] not in nlist:
-                    nlist.update({t.attrib['form']:int(1)})
+                if t.attrs['form'] not in nlist:
+                    nlist.update({t.attrs['form']:int(1)})
                 else:
-                    nlist[t.attrib['form']]+=1
+                    nlist[t.attrs['form']]+=1
         except:
-            exceptions += t.attrib['form']
-            exceptions += ' '
+            exceptions+=t.attrs['form']
+            exceptions+=' '
 defaultNumb=1
 
 result=str()
@@ -36,8 +38,8 @@ for key in nlist:
     print ('у слова ', key,' количество вхождений равно',nlist[key])
 for key in nlist:
     if int(nlist[key]) > 1:
-        result +=str(key.encode('utf-8'))
-        result +=' ,'
+        result +=str(key)
+        result +=', '
 print ('\nКлючевыми в этом текте являются слова: ')
 print (result)
 print ('Неизвестные слова:')
